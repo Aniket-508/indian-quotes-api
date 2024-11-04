5474a4f7ab0bfec584f9b9368ebdfd0b8ece77d1
Aniket-508 <pawaraniket508@gmail.com>
2024-11-05 00:01:55 +0530

    fix: titleCase fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,8 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function titleCase(val: string) {
   // https://stackoverflow.com/questions/32589197/how-can-i-capitalize-the-first-letter-of-each-word-in-a-string-using-javascript
-  let splitStr = val.toLowerCase().split("_");
-  for (var i = 0; i < splitStr.length; i++) {
+  const splitStr = val.toLowerCase().split("_");
+  for (let i = 0; i < splitStr.length; i++) {
     splitStr[i] =
       splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
   }

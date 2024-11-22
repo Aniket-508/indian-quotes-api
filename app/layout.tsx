c5a9233e3c4e30ab4a621e58ac616649806bdeef
Aniket-508 <pawaraniket508@gmail.com>
2024-11-22 22:50:31 +0530

    chore: removed fonts export

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,19 @@ import { Toaster } from "@/components/ui/sonner";
 import { MAIN_METADATA } from "@/lib/meta";
 import "./globals.css";
 
-export const geistSans = localFont({
+const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 
-export const geistMono = localFont({
+const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
-export const bricolage = Bricolage_Grotesque({
+const bricolage = Bricolage_Grotesque({
   subsets: ["latin"],
   display: "swap",
   weight: ["200", "300", "400", "500", "600", "700", "800"],

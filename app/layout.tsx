49565f37c7b10b94bce565d463e92b48390bd98f
Aniket-508 <pawaraniket508@gmail.com>
2024-11-22 22:50:01 +0530

    revert: fonts lib refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,29 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Bricolage_Grotesque } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-import { geistSans, geistMono, bricolage } from "@/lib/fonts";
 import { MAIN_METADATA } from "@/lib/meta";
 import "./globals.css";
+
+export const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+export const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const bricolage = Bricolage_Grotesque({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  style: ["normal"],
+  variable: "--font-bricolage",
+});
 
 export const metadata: Metadata = {
   title: MAIN_METADATA.TITLE,
